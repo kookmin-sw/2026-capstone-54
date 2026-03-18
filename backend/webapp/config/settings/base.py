ad07a6c6e6c0c7f41764d38756b9f342cc55e9e8
shinkeonkim <dev.shinkeonkim@gmail.com>
2026-03-18 20:34:59 +0900

    refactor: django base setting을 각 역할별로 파일을 분리하여 관리하도록 함

--- backend/webapp/config/settings/base.py
+++ backend/webapp/config/settings/base.py
@@ -10,133 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
-
-import environ
-
-env = environ.Env()
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-SECRET_KEY = env("SECRET_KEY")
-DEBUG = env.bool("DEBUG", default=False)
+from .components.auth import *
+from .components.common import *
+from .components.database import *
+from .components.file import *
+from .components.i18n import *
+from .components.installed_app import *
+from .components.middleware import *
+from .components.template import *
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
-# Application definition
-
-INSTALLED_APPS = [
-  'django.contrib.admin',
-  'django.contrib.auth',
-  'django.contrib.contenttypes',
-  'django.contrib.sessions',
-  'django.contrib.messages',
-  'django.contrib.staticfiles',
-]
-
-MIDDLEWARE = [
-  'django.middleware.security.SecurityMiddleware',
-  'django.contrib.sessions.middleware.SessionMiddleware',
-  'django.middleware.common.CommonMiddleware',
-  'django.middleware.csrf.CsrfViewMiddleware',
-  'django.contrib.auth.middleware.AuthenticationMiddleware',
-  'django.contrib.messages.middleware.MessageMiddleware',
-  'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
 ROOT_URLCONF = 'config.urls'
-
-TEMPLATES = [
-  {
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [],
-    'APP_DIRS': True,
-    'OPTIONS': {
-      'context_processors': [
-        'django.template.context_processors.debug',
-        'django.template.context_processors.request',
-        'django.contrib.auth.context_processors.auth',
-        'django.contrib.messages.context_processors.messages',
-      ],
-    },
-  },
-]
-
-WSGI_APPLICATION = 'config.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASE_ENGINE = env.str("DATABASE_ENGINE", default="psqlextra.backend")
-DATABASE_NAME = env.str("DATABASE_NAME")
-POSTGRES_USER = env.str("POSTGRES_USER")
-POSTGRES_PASSWORD = env.str("POSTGRES_PASSWORD")
-POSTGRES_HOST = env.str("POSTGRES_HOST")
-POSTGRES_PORT = env.str("POSTGRES_PORT", default="5432")
-
-DATABASES = {
-  "default": {
-    "ENGINE": DATABASE_ENGINE,
-    "NAME": DATABASE_NAME,
-    "USER": POSTGRES_USER,
-    "PASSWORD": POSTGRES_PASSWORD,
-    "HOST": POSTGRES_HOST,
-    "PORT": POSTGRES_PORT,
-    "ATOMIC_REQUESTS": True,
-    "CONN_MAX_AGE": 600,  # 10분 connection pooling
-    "OPTIONS": {
-      "connect_timeout": 10,
-      "options": "-c statement_timeout=30000",  # 30초 쿼리 타임아웃
-    },
-    "TEST": {
-      "NAME": env("TEST_DATABASE_NAME", default=""),
-      "USER": env("TEST_POSTGRES_USER", default=""),
-      "PASSWORD": env("TEST_POSTGRES_PASSWORD", default=""),
-      "HOST": env("TEST_POSTGRES_HOST", default=""),
-      "PORT": env("TEST_POSTGRES_PORT", default=""),
-    },
-  }
-}
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-  {
-    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-  },
-  {
-    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-  },
-  {
-    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-  },
-  {
-    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-  },
-]
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ backend/webapp/config/settings/components/middleware.py
@@ -0,0 +1,17 @@
+"""
+Django Middleware Settings
+"""
+
+MIDDLEWARE = [
+  'django.middleware.security.SecurityMiddleware',
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+__all__ = [
+  "MIDDLEWARE",
+]
