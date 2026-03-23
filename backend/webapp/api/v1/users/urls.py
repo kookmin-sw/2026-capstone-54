98a0737b8d1a27a99d4fd6589446d81c5fb5b9d3
shinkeonkim <dev.shinkeonkim@gmail.com>
2026-03-23 20:38:15 +0900

    feat: sign-up API 추가 및 테스트 코드 작성

--- backend/webapp/api/v1/users/urls.py
+++ backend/webapp/api/v1/users/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import (
   SignInAPIView,
+  SignUpAPIView,
 )
 
 urlpatterns = [
+  path("sign-up", SignUpAPIView.as_view(), name="sign-up"),
   path("sign-in", SignInAPIView.as_view(), name="sign-in"),
 ]
