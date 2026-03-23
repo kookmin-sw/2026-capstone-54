803d77cac3ea663b9e14856b385f37a1c7f7b39a
shinkeonkim <dev.shinkeonkim@gmail.com>
2026-03-23 20:41:08 +0900

    feat: unregister API(회원탈퇴) 추가 및 테스트 코드 작성

--- backend/webapp/api/v1/users/urls.py
+++ backend/webapp/api/v1/users/urls.py
@@ -5,12 +5,14 @@ from .views import (
   SignInAPIView,
   SignOutAPIView,
   SignUpAPIView,
+  UnregisterAPIView,
 )
 
 urlpatterns = [
   path("sign-up", SignUpAPIView.as_view(), name="sign-up"),
   path("sign-in", SignInAPIView.as_view(), name="sign-in"),
   path("sign-out", SignOutAPIView.as_view(), name="sign-out"),
+  path("unregister", UnregisterAPIView.as_view(), name="unregister"),
   path("tokens/verify", TokenVerifyView.as_view(), name="token-verify"),
   path("tokens/refresh", TokenRefreshView.as_view(), name="token-refresh"),
 ]
