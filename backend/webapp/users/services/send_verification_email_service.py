cdbd0d6ed42ca4d3b9c9b3b90ea98d5cf6198c81
shinkeonkim <dev.shinkeonkim@gmail.com>
2026-03-23 20:02:35 +0900

    feat: 인증 코드 전송 서비스 로직 추가

---
+++ backend/webapp/users/services/send_verification_email_service.py
@@ -0,0 +1,44 @@
+import random
+import string
+from datetime import timedelta
+
+from common.services import BaseService
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils import timezone
+
+
+class SendVerificationEmailService(BaseService):
+  """6자리 OTP 코드를 생성하고 이메일로 발송한다."""
+
+  def execute(self):
+    from users.models import EmailVerificationCode
+
+    user = self.user
+    expiry_minutes = getattr(settings, "EMAIL_VERIFICATION_CODE_EXPIRY_MINUTES", 10)
+    code = self._generate_code()
+    expires_at = timezone.now() + timedelta(minutes=expiry_minutes)
+    EmailVerificationCode.objects.create(user=user, code=code, expires_at=expires_at)
+    self._send_email(user, code)
+    return code
+
+  def _generate_code(self):
+    charset = string.ascii_uppercase + string.digits
+    return "".join(random.choices(charset, k=6))
+
+  def _send_email(self, user, code):
+    html_message = render_to_string(
+      "users/email/verification_code.html",
+      {
+        "code": code,
+        "user_email": user.email
+      },
+    )
+    send_mail(
+      subject="이메일 인증 코드",
+      message=f"인증 코드: {code}",
+      from_email=settings.DEFAULT_FROM_EMAIL,
+      recipient_list=[user.email],
+      html_message=html_message,
+    )
