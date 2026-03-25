130e14679d76af5d7b081d5f9aa2d0e474f2e115
shinkeonkim <dev.shinkeonkim@gmail.com>
2026-03-26 00:38:01 +0900

    feat: 배포 관련 package 추가

--- backend/webapp/config/settings/production.py
+++ backend/webapp/config/settings/production.py
@@ -6,3 +6,11 @@ SIMPLE_JWT = {
   **SIMPLE_JWT,  # noqa: F405
   "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
 }
+
+# ── django-allow-cidr ──
+ALLOWED_CIDR_NETS = ["10.42.0.0/16"]
+
+MIDDLEWARE = [
+  "allow_cidr.middleware.AllowCIDRMiddleware",
+  *MIDDLEWARE,  # noqa: F405
+]
