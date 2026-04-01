c1f2a8d84dec7c24bfa8df3429b076bf2e900aa9
shinkeonkim <dev.shinkeonkim@gmail.com>
2026-04-02 01:53:30 +0900

    feat: demo scrape admin api

--- backend/webapp/api/v1/urls.py
+++ backend/webapp/api/v1/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
 urlpatterns = [
+  path("demo/", include("api.v1.demo.urls")),
   path("health-check/", include("api.v1.health_check.urls")),
   path("users/", include("api.v1.users.urls")),
   path("realtime/", include("api.v1.realtime.urls")),
