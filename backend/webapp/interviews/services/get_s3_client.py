ab65342e9f4497830a021d22a4241970b033a6ef
shinkeonkim <dev.shinkeonkim@gmail.com>
2026-04-20 03:50:56 +0900

    refactor: S3 client 두 개 모두 싱글톤 캐싱 + _build_client 공통화
    - _s3_client: 내부 통신용 (upload_part, create_multipart, put_object)
    - _s3_presign_client: presigned URL 생성용 (playback GET URL)
    - 둘 다 프로세스 수명 동안 1회만 생성, 이후 재사용
    - _build_client() 공통 헬퍼로 중복 제거

--- backend/webapp/interviews/services/get_s3_client.py
+++ backend/webapp/interviews/services/get_s3_client.py
@@ -3,35 +3,35 @@ from botocore.config import Config
 from django.conf import settings
 
 _PATH_STYLE = Config(s3={"addressing_style": "path"})
-_video_s3_client = None
+
+_s3_client = None
+_s3_presign_client = None
 
 
-def get_video_s3_client():
-  global _video_s3_client
-  if _video_s3_client is not None:
-    return _video_s3_client
-
+def _build_client(endpoint=None):
   kwargs = {"region_name": settings.AWS_S3_REGION_NAME}
-  endpoint = getattr(settings, "VIDEO_S3_ENDPOINT_URL", None) or getattr(settings, "AWS_S3_ENDPOINT_URL", None)
   if endpoint:
     kwargs["endpoint_url"] = endpoint
     kwargs["aws_access_key_id"] = "dummy"
     kwargs["aws_secret_access_key"] = "dummy"
     kwargs["config"] = _PATH_STYLE
+  return boto3.client("s3", **kwargs)
 
-  _video_s3_client = boto3.client("s3", **kwargs)
-  return _video_s3_client
+
+def get_video_s3_client():
+  global _s3_client
+  if _s3_client is None:
+    endpoint = getattr(settings, "VIDEO_S3_ENDPOINT_URL", None) or getattr(settings, "AWS_S3_ENDPOINT_URL", None)
+    _s3_client = _build_client(endpoint)
+  return _s3_client
 
 
 def get_video_s3_presign_client():
-  public_endpoint = getattr(settings, "VIDEO_S3_PUBLIC_ENDPOINT_URL", None)
-  if public_endpoint:
-    return boto3.client(
-      "s3",
-      region_name=settings.AWS_S3_REGION_NAME,
-      endpoint_url=public_endpoint,
-      aws_access_key_id="dummy",
-      aws_secret_access_key="dummy",
-      config=_PATH_STYLE,
-    )
-  return get_video_s3_client()
+  global _s3_presign_client
+  if _s3_presign_client is None:
+    endpoint = getattr(settings, "VIDEO_S3_PUBLIC_ENDPOINT_URL", None)
+    if endpoint:
+      _s3_presign_client = _build_client(endpoint)
+    else:
+      _s3_presign_client = get_video_s3_client()
+  return _s3_presign_client
