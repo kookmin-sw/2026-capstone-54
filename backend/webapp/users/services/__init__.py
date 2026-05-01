0e4e3214e6b3a5a99feaf195ca71427eb9f48581
shinkeonkim <dev.shinkeonkim@gmail.com>
2026-05-01 12:11:06 +0900

    feat: 로그인 사용자 이름 수정 기능 추가
    • UpdateUserNameSerializer 추가로 이름 변경을 위한 입력값 검증 구현
    • UserMeAPIView에 PATCH 메서드 추가하여 이름 수정 API 지원
    • UpdateUserNameService 추가로 사용자 이름 변경 로직 분리 및 적용
    • 관련 테스트 케이스 작성으로 정상 동작 및 예외 상황 검증
    • 이 기능을 통해 인증된 사용자는 자신의 이름을 안전하게 변경 가능

--- backend/webapp/users/services/__init__.py
+++ backend/webapp/users/services/__init__.py
@@ -7,6 +7,7 @@ from .send_verification_email_service import SendVerificationEmailService
 from .sign_in_service import SignInService
 from .sign_out_service import SignOutService
 from .sign_up_service import SignUpService
+from .update_user_name_service import UpdateUserNameService
 from .user_masking_service import UserMaskingService
 from .verify_email_service import VerifyEmailService
 
@@ -20,6 +21,7 @@ __all__ = [
   "SignInService",
   "SignOutService",
   "SignUpService",
+  "UpdateUserNameService",
   "UserMaskingService",
   "VerifyEmailService",
 ]
