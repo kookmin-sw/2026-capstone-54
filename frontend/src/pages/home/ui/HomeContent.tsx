a6e311c4583fcfd40df72a18c75cba4f9d7a72fc
joan4720 <joan030225@kookmin.ac.kr>
2026-05-01 09:07:37 +0900

    feat: add hideIcon prop to StreakCalendar for home page display
    - Add hideIcon prop to StreakCalendar to hide icon and increase title
      font size to 16px when shown on home page
    - Pass hideIcon to StreakCalendar in HomeContent

--- frontend/src/pages/home/ui/HomeContent.tsx
+++ frontend/src/pages/home/ui/HomeContent.tsx
@@ -63,6 +63,7 @@ export function HomeContent() {
                 todayYear={todayY}
                 todayMonth={todayM}
                 todayDay={todayD}
+                hideIcon
               />
             </div>
           )}
